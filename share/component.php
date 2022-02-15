b38e4f739378d5f68074e36012c90278d804f068
matin <matin@gmail.com>
2022-02-15 11:43:06 -0500

    add viewport and charset meta tag to component file

--- share/component.php
+++ share/component.php
@@ -107,6 +107,8 @@
         static function headFiles () {
 
 ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="dns-prefetch" href="http://localhost">
 
