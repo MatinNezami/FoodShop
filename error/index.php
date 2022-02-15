be946e1f511f799144074fb48d700051502a4def
matin <matin@gmail.com>
2022-02-15 12:04:14 -0500

    create the not found page files

---
+++ error/index.php
@@ -0,0 +1,18 @@
+<?php require_once "../share/component.php" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Not Found</title>
+
+    <?php component::headFiles() ?>
+</head>
+
+<body>
+    <header>
+        <?php component::navbar() ?>
+    </header>
+</body>
+
+</html>
