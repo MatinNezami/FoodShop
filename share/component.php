4d8fe92660e8f3a48e8be0b30ea73d9242146e14
matin <matin@gmail.com>
2022-02-15 13:28:21 -0500

    add robots meta tag (SEO)

--- share/component.php
+++ share/component.php
@@ -109,6 +109,7 @@
 ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="follow index">
 
     <link rel="dns-prefetch" href="http://localhost">
 
