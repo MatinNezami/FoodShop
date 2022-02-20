eea3df80b7c5de67671793dac86b201d6a6218db
matin <matin@gmail.com>
2022-02-20 14:45:10 -0500

    remove robots meta tag ===> default index follow (SEO)

--- share/component.php
+++ share/component.php
@@ -90,7 +90,6 @@
 ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="robots" content="follow index">
 
     <link rel="dns-prefetch" href="http://localhost">
 
