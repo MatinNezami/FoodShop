c71f37cef1f6829eef6694611302cc6263df0b02
matin <matin@gmail.com>
2022-03-08 11:54:30 -0500

    add users table in mysql

---
+++ sql/users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+    `username` VARCHAR(30) PRIMARY KEY,
+    `email` VARCHAR(255) UNIQUE NOT NULL,
+    `accept` TINYINT(1) NOT NULL DEFAULT(0),
+    `password` VARCHAR(30) NOT NULL,
+    `firstName` VARCHAR(30) NOT NULL,
+    `profile` TINYBLOB NOT NULL
+);
