c4e2026a680a1feb31fbd8c5fb19f7e32c9133d2
matin <matin@gmail.com>
2022-03-10 08:42:02 -0500

    use up version client validate

--- share/validate.js
+++ share/validate.js
@@ -5,6 +5,7 @@ class Validate {
     ok;
     inputs = {};
     emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    samePassword;
 
     checkEmpty = input => input.value;
 
@@ -43,6 +44,11 @@ class Validate {
                 status: false,
                 message: "password is not strong"
             };
+
+        if (!this.samePassword)
+            return {
+                status: true
+            }
 
         for (let item of input.value.toLowerCase().match(/.{1,3}/g)?? [])
             if (username.value.toLowerCase().includes(item))
@@ -119,7 +125,9 @@ class Validate {
         return new FormData(form);
     }
 
-    constructor (form) {
+    constructor (form, samePassword = true) {
+        this.samePassword = samePassword;
+
         form.querySelectorAll("input").forEach(
             input => this.inputs[input.name] = input
         );
