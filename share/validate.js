1e7bfae37050dcf2ef52aa431e5c1577455f8fd3
matinnezami <matin912nezami@gmail.com>
2022-03-30 15:59:58 -0400

    use new version client validate

--- share/validate.js
+++ share/validate.js
@@ -7,8 +7,6 @@ class Validate {
     emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
     samePassword;
 
-    checkEmpty = input => input.value;
-
     simple = input => ({
         status: new RegExp(`^.{${input.minLength? 5: null},${input.maxLength? 30: null}}$`).test(input.value)
     });
@@ -109,17 +107,19 @@ class Validate {
 
     validate (form) {
         for (let input in this.inputs) {
-            const empty = this.checkEmpty(this.inputs[input]),
-                validate = this.checkData(this.inputs[input]);
+            input = this.inputs[input];
 
-            if (this.inputs[input].required && !empty)
-                return Validate.error(this.inputs[input], "input is empty");
+            if (input.required && !input.value)
+                return Validate.error(input, "input is empty");
 
-            if (empty && validate.status) continue;
+            if (!input.required && !input.value) continue;
+
+            const validate = this.checkData(input);
+
+            if (input.value && validate.status) continue;
                 
-            let message = validate.message?? `${input} ${this.details? "isn't valid": "didn't match"}`;
-
-            return Validate.error(this.inputs[input], message.replaceAll("-", " "));
+            let message = validate.message?? `${input.name} ${this.details? "isn't valid": "didn't match"}`;
+            return Validate.error(input, message.replaceAll("-", " "));
         }
 
         return new FormData(form);
