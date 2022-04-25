b084663653bddda4fc35211c1593cd0af5a7d68c
matinnezami <matin912nezami@gmail.com>
2022-04-25 15:38:45 -0400

    static center products

--- index.php
+++ index.php
@@ -92,58 +92,72 @@
         </div>
 
 
-        <div class="effect-products-box">
-            <div class="desktop center-item">
-                <div class="left center-item">
-                    <a href="#" class="aside-product left center-item">
-                        <div class="info center-item">
-                            <strong>Sunglasses</strong>
-                            <p>300000</p>
-                        </div>
-            
-                        <img src="/images/product/7.webp" loading="lazy" alt="product picture" draggable="false">
-                    </a>
+        <div class="center-item products-box effect">
+            <a href="#" class="product center-item">
+                <div class="image">
+                    <img src="/images/product/9.webp" loading="lazy" alt="product picture" draggable="false">
                 </div>
-
-                <div class="right center-item">
-                    <a href="#" class="aside-product right center-item">
-                        <img src="/images/product/8.webp" loading="lazy" alt="product picture" draggable="false">
-                        
-                        <div class="info center-item">
-                            <strong>T Shirt</strong>
-                            <p>60000</p>
-                        </div>
-                    </a>
+    
+                <div class="info center-item">
+                    <strong>T Shirt</strong>
+                    <p>50000</p>
                 </div>
-            </div>
-
-            <div class="mobile center-item">
-                <div class="center-item">
-                    <a href="#" class="hide-product center-item">
-                        <div class="image">
-                            <img src="/images/product/16.webp" loading="lazy" alt="product picture" draggable="false">
-
-                            <div class="info center-item">
-                                <strong>Nike</strong>
-                                <p>300000</p>
-                            </div>
-                        </div>
-                    </a>
+            </a>
+    
+            <a href="#" class="product center-item">
+                <div class="image">
+                    <img src="/images/product/10.webp" loading="lazy" alt="product picture" draggable="false">
                 </div>
-
-                <div class="center-item">
-                    <a href="#" class="hide-product center-item">
-                        <div class="image">
-                            <img src="/images/product/15.webp" loading="lazy" alt="product picture" draggable="false">
-
-                            <div class="info center-item">
-                                <strong>Tennis Hat</strong>
-                                <p>15000</p>
-                            </div>
-                        </div>
-                    </a>
+    
+                <div class="info center-item">
+                    <strong>Man Shoe</strong>
+                    <p>320000</p>
                 </div>
-            </div>
+            </a>
+    
+            <a href="#" class="product center-item">
+                <div class="image">
+                    <img src="/images/product/11.webp" loading="lazy" alt="product picture" draggable="false">
+                </div>
+    
+                <div class="info center-item">
+                    <strong>Sunglasses</strong>
+                    <p>20000</p>
+                </div>
+            </a>
+    
+            <a href="#" class="product center-item">
+                <div class="image">
+                    <img src="/images/product/12.webp" loading="lazy" alt="product picture" draggable="false">
+                </div>
+    
+                <div class="info center-item">
+                    <strong>Woman's Shoe</strong>
+                    <p>90000</p>
+                </div>
+            </a>
+    
+            <a href="#" class="product center-item">
+                <div class="image">
+                    <img src="/images/product/13.webp" loading="lazy" alt="product picture" draggable="false">
+                </div>
+    
+                <div class="info center-item">
+                    <strong>Scarf</strong>
+                    <p>18000</p>
+                </div>
+            </a>
+    
+            <a href="#" class="product center-item">
+                <div class="image">
+                    <img src="/images/product/14.webp" loading="lazy" alt="product picture" draggable="false">
+                </div>
+    
+                <div class="info center-item">
+                    <strong>Gloves</strong>
+                    <p>8000</p>
+                </div>
+            </a>
         </div>
     </main>
 
